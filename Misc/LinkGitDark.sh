4e1edb45c74dc9ca41728872e98e19c74b20581a
kanehekili <kanehekili@msi>
2021-10-23 22:54:28 +0200

    CSD Shadows, Revealer, round switch

---
+++ Misc/LinkGitDark.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+ln -s ~/git/Gradient-blue/Cinnamon/cinnamon cinnamon
+ln -s ~/git/Gradient-blue/GTK-3.22-dark/src/Gradient-black/gnome-shell gnome-shell
+ln -s ~/git/Gradient-blue/GTK-3.22-dark/src/Gradient-black/gtk-2.0 gtk-2.0
+ln -s ~/git/Gradient-blue/GTK-3.22-dark/src/Gradient-black/gtk-3.0 gtk-3.0
+ln -s ~/git/Gradient-blue/metacity/Gradient-black/metacity-1 metacity-1
+ln -s ~/git/Gradient-blue/xfwm4/Gradient-black/xfce-notify-4.0 xfce-notify-4.0
+ln -s ~/git/Gradient-blue/xfwm4/Gradient-black/xfwm4 xfwm4
+ln -s ~/git/Gradient-blue/GTK-3.22-dark/src/Gradient-black/index.theme index.theme
+ln -s /home/matze/git/Gradient-blue/metacity/Gradient-black/metacity-1 gtk-3.0/csd
